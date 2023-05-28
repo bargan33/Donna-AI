d8470d1ea91e3b900483ee3507f59d5030882669
Jan Barganowski <jbarganowski@gmail.com>
2023-05-28 11:14:57 +0200

    Add Forms Caller

---
+++ src/forms_caller.py
@@ -0,0 +1,41 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from typing import List, Tuple
+
+
+class FormsCaller:
+    def __init__(self, form_id: str, credentials_file_path='keys/google_api_key.json'):
+        self.form_id = form_id  # ID of your Google Form
+        scopes = ['https://www.googleapis.com/auth/forms',
+                  'https://www.googleapis.com/auth/drive']
+
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials_file_path, scopes=scopes
+        )
+
+        self.service = build('forms', 'v1', credentials=credentials)
+
+    def __query_questions(self) -> dict:
+        return self.service.forms().get(formId=self.form_id).execute()
+
+    def __parse_questions(self) -> dict:
+        questions = {}
+        for item in self.__query_questions()['items']:
+            questions[item["questionItem"]['question']
+                      ["questionId"]] = item["title"]
+        return questions
+
+    def __query_answers(self) -> dict:
+        return self.service.forms().responses().list(formId=self.form_id).execute()
+
+    def get_qa(self) -> List[List[Tuple[str, str]]]:
+        questions = self.__parse_questions()
+        responses = self.__query_answers()['responses']
+        entries = []
+        for response in responses:
+            paired_qa = []
+            for q in questions:
+                paired_qa.append(
+                    (questions[q], response['answers'][q]['textAnswers']['answers'][0]['value']))
+            entries.append(paired_qa)
+        return entries

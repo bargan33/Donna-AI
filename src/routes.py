71aac20abb944d8ebd9417d87f892ea1e6ef4344
Jan Barganowski <jbarganowski@gmail.com>
2023-05-28 12:17:49 +0200

    Add new session view, set up flask routing

--- src/routes.py
+++ src/routes.py
@@ -9,3 +9,8 @@ bp = Blueprint('routes', __name__)
 @bp.route('/')
 def home():
     return render_template('index.html')
+
+
+@bp.route('/new_session')
+def new_session():
+    return render_template('new_session.html')
